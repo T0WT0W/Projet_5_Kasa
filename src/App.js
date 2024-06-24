48d3f7b112a7e59d301977bed98fb743f9f7ba81
Thomas Pierre <tomlo4@hotmail.com>
2024-06-24 14:32:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios'
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Housing from './pages/Housing';
+import Error from './pages/Error';
+import Footer from './pages/Footer';
 
 function App() {
+
+  const [pageActive, modifierPageActive] = useState("Home");
+  const [logements, modifierLogements] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/logements.json", {
+      responseEncoding: "utf8"
+    })
+      .then((results) => {
+        modifierLogements((l) => results.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar pageActive={pageActive} />
+        <div className="top">
+          <Routes>
+            <Route index path='/' element={<Home logements={logements} modifierPageActive={modifierPageActive} />} />
+            <Route path='/about' element={<About modifierPageActive={modifierPageActive} />} />
+            <Route path="/housing/:houseId" element={<Housing logements={logements} modifierPageActive={modifierPageActive} />} />
+            <Route path='*' element={<Error modifierPageActive={modifierPageActive} />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
